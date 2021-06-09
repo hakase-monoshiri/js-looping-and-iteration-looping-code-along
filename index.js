9c58b96348149a99162bb9bee2ac269bc39c5504
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-09 16:29:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (namesArray, eventName) {
+    let newNames = []
+    for ( let i = 0; i < namesArray.length; i ++ ) {
+        let name = namesArray[i];
+        newNames.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`);
+    }
+    return newNames
+}
+
+function countDown (number) {
+    let i = number
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
